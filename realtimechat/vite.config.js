aaf39846d7d910e0d82548c0e69d6cba6461324f
rabindra-basnet <120715649+rabindra-basnet@users.noreply.github.com>
2025-09-19 14:16:19 +0545

    Delete realtimechat directory

--- realtimechat/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [tailwindcss()],
-});
